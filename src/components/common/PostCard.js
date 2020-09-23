9690cf5af0829c946763cb43a7036ac53b744eff
Edgar Giovanni Lepe <lepe.edgar10@gmail.com>
2020-09-23 12:54:33 -0700

    Remove image from post cards

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -13,25 +13,12 @@ const PostCard = ({ post }) => {
     return (
         <Link to={url} className={postCardStyles.postCard}>
             <header className={postCardStyles.postCardHeader}>
-                {post.feature_image &&
-                    <div className={postCardStyles.postCardImage} style={{
-                        backgroundImage: `url(${post.feature_image})` ,
-                    }}></div>}
-                {post.tags && <div className={postCardStyles.postCardTags}> <Tags post={post} visibility="public" autolink={false} /></div>}
-                {post.featured && <span>Featured</span>}
                 <h2 className={postCardStyles.postCardTitle}>{post.title}</h2>
+                {post.featured && <span>Featured</span>}
+                {post.tags && <div className={postCardStyles.postCardTags}> <Tags post={post} visibility="public" autolink={false} /></div>}
             </header>
             <section className={postCardStyles.postCardExcerpt}>{post.excerpt}</section>
             <footer className={postCardStyles.postCardFooter}>
-                <div className={postCardStyles.postCardFooterLeft}>
-                    <div className={postCardStyles.postCardAvatar}>
-                        {post.primary_author.profile_image ?
-                            <img className={postCardStyles.authorProfileImage} src={post.primary_author.profile_image} alt={post.primary_author.name}/> :
-                            <img className={postCardStyles.defaultAvatar} src="/images/icons/avatar.svg" alt={post.primary_author.name}/>
-                        }
-                    </div>
-                    <span>{ post.primary_author.name }</span>
-                </div>
                 <div className={postCardStyles.postCardFooterRight}>
                     <div>{readingTime}</div>
                 </div>
